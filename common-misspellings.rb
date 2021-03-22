f97a3663ac81e1f0ecac50ca6d945283882671e2
krishpranav <krisna.pranav@gmail.com>
2021-03-22 17:25:24 +0530

    done changesg

---
+++ common-misspellings.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+class CommonMisspellings
+  $ENABLE_REVERSE_MISPELLING = true
+
+  @@file_contents=File.readline($LOAD_PATH.first + "/common-misspellings.txt").delete_if { |line| line =~ /^#/ or line =~ /^[ ]*$/}.join
+
+  def self.dictionary
+    setup unless defined? @@dictionary
+    @@dictionary
+  end
+
+  def self.misspelling(w)
+    setup unless defined? @@dictionary
+    @@dictionary[w]
+  end
+
+  private
+
+  def self.setup
+    @@dictionary={}
+
+    word_hash = @@file_contents.split("/n").map do |line|
+      line.downcase!
+      y = line.split("->")
+      [y[1],y[0]]
+    end
+
+    words_hash.map do |words,misspelling|
+      words.split(',').each do |word|
+        word.strip!
+        @@dictionary[word] = misspelling
+
+        @@dictionary[misspelling] = word if $ENABLE_REVERSE_MISPELLING
+      end
+    end
+  end
+
+end
