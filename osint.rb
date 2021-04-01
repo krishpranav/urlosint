8af302335db6ccd429c8655db93a200ae28d6418
krishpranav <krisna.pranav@gmail.com>
2021-04-01 14:10:46 +0530

    done change

--- osint.rb
+++ osint.rb
@@ -39,4 +39,114 @@ class Start
     $choice=gets.chomp.to_i
   end
 
-  
+  def main
+
+
+    if $choice==1
+      puts "Subdomain Enumeration:>".green
+      puts "Enter your Target (ex: 'google.com'): ".green
+      target=gets.chomp.to_s
+      subenum=Subenum.new
+      subenum.attack(target)
+      puts ""
+      puts "[+] Results saved to #{target}.txt"
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==2
+      puts "Subdomain BruteForce:>".green
+      puts "Enter your Target (ex: 'google.com'): ".green
+      domain=gets.chomp.to_s
+      sub=Subs.new
+      sub.brute(domain)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+
+    elsif $choice==3
+      puts "live domain checker:>".green
+      puts "Enter subdomains list directory(ex: /home/root/subdomains.txt):".green
+      list=gets.chomp
+      live=Domain.new
+      live.checker(list)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+
+    elsif $choice==4
+      puts "Resolve domains to ips:>".green
+      puts "Enter subdomains list directory(ex: /home/root/subdomains.txt):".green
+      s_list=gets.chomp
+      resolve=Resolver.new
+      resolve.hosts(s_list)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==5
+      puts "Resolve ips to domains:>".green
+      puts "Enter IPS list directory(ex: /home/root/IPS.txt):".green
+      i_list=gets.chomp.to_s
+      ip=Domain.new
+      ip.ips(i_list)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==6
+      puts "Directory BruteForce:>".green
+      puts "Enter your Target Domain or Domain with Directory(ex: 'google.com' or a directory 'google.com/profiles'): ".green
+      host=gets.chomp.to_s
+      puts"Enter Target Protocol(ex: 'http' or 'https'): ".green
+      protocol=gets.chomp.to_s
+      puts "Enter Wordlist file(ex: php.txt): ".green
+      file=gets.chomp.to_s
+      brute=Direr.new
+      brute.attack(host,protocol,file)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==7
+      puts ""
+      puts "Extract Web Headers:>".green
+      puts "Enter subdomains list(ex: subdomains.txt): ".green
+      sublist=gets.chomp.to_s
+      puts ""
+      header=Web.new
+      puts header.grab(sublist)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+    elsif $choice==8
+      puts ""
+      puts "Shodan Searcher:>".green
+      puts "Enter Your target doamin(ex: google.com):".green
+      target=gets.chomp.to_s
+      shod=Shodan.new
+      puts shod.attack(target)
+      puts ""
+      puts "[+] Press Enter to Continue: ".blue
+      cont=gets.chomp
+      system('clear') || system('cls')
+
+
+    end
+  end
+end
+
+while TRUE
+    run=Start.new
+    run.webrub
+    run.main
+end
